85171970e60de11db56da0a506824648fd2a89ce
Sadumi <110389670+Sadumi@users.noreply.github.com>
2023-04-13 07:20:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
+global.owner = ['94755805957']
 global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.ownernomer = '94755805957'
+global.ownername = 'Dinu'
+global.botname = 'BIXBY-MD Dinu BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
